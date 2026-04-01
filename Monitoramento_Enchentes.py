ffe8416a8c0fe757743ba0c8bfa1a40a00901afd
Enzo Ferreira <enzocafaria@gmail.com>
2026-04-01 15:53:22 -0300

    criando coletor de niveis

--- Monitoramento_Enchentes.py
+++ Monitoramento_Enchentes.py
@@ -11,3 +11,19 @@ def exibir_menu():
     print("=" * 60)
     print("🛰️  Monitoramento de Enchentes - Aqua Intel")
     print("=" * 60)
+
+
+def coletar_niveis(dias):
+    niveis = []
+    for dia in range(1, dias + 1):
+        while True:
+            try:
+                nivel = float(input(f"🌊 Digite o nível do rio no dia {dia} (em metros): "))
+                if nivel < 0:
+                    print("❌ O nível não pode ser negativo. Tente novamente.")
+                else:
+                    niveis.append(nivel)
+                    break
+            except ValueError:
+                print("❌ Valor inválido. Digite um número.")
+    return niveis
