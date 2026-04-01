afb9093846fff01a0b0a40e42f8cdb5bb2442140
Guilherme de Paula Kuskowski <guilhermekuskowski0@gmail.com>
2026-04-01 15:51:40 -0300

    criando menu

---
+++ Monitoramento_Enchentes.py
@@ -0,0 +1,13 @@
+#Enzo de Faria Ferreira / 562448
+#Guilherme Eduardo de Lima / 566045
+#Guilherme de Paula Kuskowski / 562471
+# -----------------------------------------
+# Projeto Aqua Intel - Monitoramento de Enchentes
+# Computational Thinking Using Python
+# Cidade: Petrópolis - RJ
+# -----------------------------------------
+
+def exibir_menu():
+    print("=" * 60)
+    print("🛰️  Monitoramento de Enchentes - Aqua Intel")
+    print("=" * 60)
